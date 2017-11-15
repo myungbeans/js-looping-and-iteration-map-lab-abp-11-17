df7fad3b62261fb2c5998fc5d6008b38e95bee91
Michael Myung <michael.h.myung@gmail.com>
2017-11-15 05:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function lowerCaseDrivers(array){
 function nameToAttributes(names){
   //names = ['Bobby Smith', 'Sammy Watkins']
   return names.map(function(name){
-    let fName=name.substring([0],myStr.indexOf(" ")
-    let lName=name.substring([0],myStr.indexOf(" ")
-    return {firstName: firstName, lastName: lName}
+    let fName=name.substring(0,myStr.indexOf(" ")
+    let lName=name.substring(0,myStr.indexOf(" ")
+    return {firstName : firstName, lastName: lName}
   })
 
 
