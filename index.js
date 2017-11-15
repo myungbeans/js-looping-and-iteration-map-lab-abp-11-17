e0a33c86f881db3c412900e285c04e6b3ca62019
Michael Myung <michael.h.myung@gmail.com>
2017-11-15 05:42:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(e){
+    return e.toLowerCase()
+  })
+}
+
+function nameToAttributes(names){
+  //names = ['Bobby Smith', 'Sammy Watkins']
+  names.map(function(name){
+    let fName=name.split(" ")[0]
+    let lName=name.split(" ")[0]
+    return {firstName: firstName, lastName: lName}
+  })
+
+
+  //[{firstName: "Bobby", lastName: "Smith"}, {firstName: "Sammy", lastName; "Watkins"}]
+}
